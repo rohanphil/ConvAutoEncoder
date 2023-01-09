d005ebd8a5af27cf56b2d80031fb5cdf97929f48
Rohan Philip <rohanphil98@gmail.com>
2023-01-08 20:32:17 -0500

    Added in the linear classifier head

--- main.py
+++ main.py
@@ -4,14 +4,32 @@ from utils import *
 from dataloaders import *
 from model import *
 from train_autoencoder import *
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 import matplotlib.pyplot as plt
+
+class LinearHead(nn.Module):
+
+	def __init__(self,input_dim):
+		super().__init__()
+		self.linearhead = nn.Sequential(
+			nn.Linear(input_dim, 32),
+			nn.ReLU(),
+			nn.Linear(32, 10)
+			)
+		self.softmax = nn.Softmax(dim = -1)
+
+	def forward(self,x):
+		return self.linearhead(x)
+
 
 test = True
 
 
-encoder = Encoder(8)
-decoder = Decoder(8)
+encoder = Encoder(100)
+decoder = Decoder(100)
 
 loss = CreateLoss().loss()
 
@@ -48,6 +66,60 @@ except:
 
 		encoder, decoder, tl = train_ae(encoder, decoder, train, 100, optim, device, loss, 30, save_model = True)
 #print(tl)
+
+def train_linearhead(encoder,device,train, test, optim, batch_size = 100, num_epochs = 10):
+
+	train_loader = dataloader(batch_size = batch_size).create_loader(train)
+	encoder.eval()
+	linear_model = LinearHead(100).to(device)
+	optim = torch.optim.Adam(linear_model.parameters(), lr = 0.01,weight_decay = 1e-02)
+	loss = nn.CrossEntropyLoss()
+
+	for n in range(num_epochs):
+		lin_train = []
+
+		for batch,labels in train_loader:
+			batch = batch.to(device)
+			encoded_data = encoder(batch).detach()
+			output = linear_model(encoded_data)
+
+			#print(type(output[0].item()))
+			#print(output)
+			l = loss(output.cpu(), labels)
+			optim.zero_grad()
+			l.backward()
+			optim.step()
+			lin_train.append(l.data)
+		print(f"linear training loss at epoch {n} = {np.mean(lin_train)}")
+
+	return (encoder, linear_model)
+
+encoder, linear_model = train_linearhead(encoder, device,train, test, optim)
+
+
+def test_acc(encoder,linear_model, test_set, batch_size):
+	acc = 0
+	n = len(test_set)
+
+	encoder.eval()
+	linear_model.eval()
+
+	test_loader = dataloader(batch_size = batch_size).create_loader(test_set)
+
+	for batch,label in test_loader:
+		batch = batch.to(device)
+		label = label.to(device)
+		encoded_data = encoder(batch).detach()
+		output = torch.argmax(linear_model(encoded_data), dim = -1)
+		acc += len(torch.where(output == label)[0])
+		#print(acc)
+	return acc/n
+
+
+
+test_acc = test_acc(encoder, linear_model, test, batch_size = 100)
+print(f"The final test accuracy = {test_acc}")
+
 
 
 if __name__ == "__main__":
